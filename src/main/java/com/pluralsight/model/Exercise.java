fb82e7719d7c09ed47df89888df1980e751efbfc
Csobban <csoban.attila@openminds.hu>
2015-06-04 13:30:11 +0200

    onetomany branch elott

--- src/main/java/com/pluralsight/model/Exercise.java
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -1,28 +1,57 @@
 package com.pluralsight.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
+@Table(name = Exercise.TABLE_NAME)
 public class Exercise {
 	
-	@Range(min = 1, max = 120)
-	private int minutes;
+	public final static String TABLE_NAME = "exercise";
+	public final static String FIELD_ID = "ID";
+	public final static String FIELD_MINUTES = "MINUTES";
 	
+	
+	@Id
+	@GeneratedValue
+	@Column(name = Exercise.FIELD_ID)
+	private Long id;
+	
+	@Range(min = 1, max = 120)
+	@Column(name = Exercise.FIELD_MINUTES)
+	private int minutes;
+
 	@NotNull
 	private String activity;
+	
+	
 
 	public String getActivity() {
 		return activity;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public int getMinutes() {
+		return minutes;
 	}
 
 	public void setActivity(String activity) {
 		this.activity = activity;
 	}
 
-	public int getMinutes() {
-		return minutes;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
