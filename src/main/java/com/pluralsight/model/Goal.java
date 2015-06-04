0596a762ea3d624677f691e7bdac7f0f4e8b0f18
Csobban <csoban.attila@openminds.hu>
2015-06-04 10:03:20 +0200

    @table added to goal entity

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -3,12 +3,16 @@ package com.pluralsight.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Range;
 
 @Entity // alap entity annotaciot hasznaljuk, ha cserelni szeretnem a providert, ne kelljen ezt is modositani
+@Table(name=Goal.TABLE_NAME)
 public class Goal {
 
+	public final static String TABLE_NAME = "goals";
+	
 	@Id
 	@GeneratedValue
 	private Long id;
