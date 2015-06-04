dac95c5bc0b099be0a93a753685cb52f5c217399
Csobban <csoban.attila@openminds.hu>
2015-06-04 09:42:12 +0200

    Goal entity class created

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -1,16 +1,31 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.Range;
 
-
-
+@Entity // alap entity annotaciot hasznaljuk, ha cserelni szeretnem a providert, ne kelljen ezt is modositani
 public class Goal {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
 	@Range(min = 1, max = 120)
 	private int minutes;
 
+	public long getId() {
+		return id;
+	}
+
 	public int getMinutes() {
 		return minutes;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
