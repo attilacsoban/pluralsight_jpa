806eb20cc05bdaeafe821e5b3b470f43d2b93e0b
Csobban <csoban.attila@openminds.hu>
2015-06-05 10:32:58 +0200

    Exercise reposetory chnged to spring data jpa

--- src/main/java/com/pluralsight/repository/ExerciseDao.java
+++ src/main/java/com/pluralsight/repository/ExerciseDao.java
@@ -1,9 +1,14 @@
 package com.pluralsight.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Exercise;
 
-public interface ExerciseDao {
+@Repository("exerciseDao")
+public interface ExerciseDao extends JpaRepository<Exercise, Long> {
 	
-	Exercise save(Exercise ex);
+	/* 1. parameter mit szolgalunk ki, 2. param az elsodleges kulcsanak tipusa*/
+	/* fater extend all of the basic function is aviable */
 	
 }
